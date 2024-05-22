f778f1ad0cf62e40cff7b1e0447661c495238743
inayat bakshi <inayatbakshi75@gmail.com>
2024-05-22 17:02:53 +0530

    add favicons

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="font-heading text-3xl">this is heading font</h1>
+<p class="font-body text-3xl">This is body font</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        heading: ["CustomFont", "sans-serif"],
+        body: ["Cabin", "sans-serif"]
+      }
+    }
   },
   plugins: []
 };
