e6d397657354998b7b2381ef78780679b3b3adf7
shahidrashid1942 <sr14201721@gmail.com>
2024-05-23 16:23:51 +0530

    add project section

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { projects } from "../../lib/data";
+    import projectData from "$lib/projectData";
 </script>
 
 <section class="p-2">
@@ -7,15 +7,15 @@
     Our Projects
 </h1>
 <div class="grid grid-cols-12 gap-4">
-{#each projects as project}
+{#each projectData as project}
     <div class="flex flex-col justify-between border border-title border-2px rounded-lg p-2 m-2 col-span-12 md:col-span-6 lg:col-span-3">
-    <img alt={project.img} src={project.img} height="250px" width="200px" class="h-auto w-auto rounded-lg" />
+    <img alt={project.title} src={project.img} height="250px" width="200px" class="h-auto w-auto rounded-lg" />
     <div class="items-baseline">
         <h2 class="text-title text-xl tracking-wide font-heading my-2">{project.title}</h2>
         <p class="text-wrap text-white p-2 text-lg font-light">{project.description}</p>
         <div>
-        <a href={project.github} target="_blank" class="px-2 py-1 border border-title rounded-lg text-title hover:bg-title hover:border-none hover:text-black">GitHub</a>
-        <a href={project.live} target="_blank" class="px-2 py-1 border border-title rounded-lg hover:text-title hover:bg-black bg-title text-black">Live</a>
+        <a href={project.githubLink} target="_blank" class="px-2 py-1 border border-title rounded-lg text-title hover:bg-title hover:border-none hover:text-black">GitHub</a>
+        <a href={project.url} target="_blank" class="px-2 py-1 border border-title rounded-lg hover:text-title hover:bg-black bg-title text-black">Live</a>
         </div>
     </div>
     </div>
